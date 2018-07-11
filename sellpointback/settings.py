5d0e47f73ed163b889589df2a85706d90e3be072
Oswaldinho24k <oswalfut_96@hotmail.com>
2018-07-10 23:30:04 -0500

    damn

--- sellpointback/settings.py
+++ sellpointback/settings.py
@@ -162,3 +162,18 @@ AUTHENTICATION_BACKENDS = (
 )
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+
+#heroku stuff
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env) 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
