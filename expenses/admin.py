4e2f798d5002633d28d938d98175779616a1e550
JoseLAmador <josel.amador.s@gmail.com>
2018-07-13 16:28:33 -0500

    list

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Expense, Provider
 
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ('provider', 'email', 'phone_number', 'owner')
+    search_fields = ('provider', 'owner__username')
+    ordering = ('-owner',)
+
 admin.site.register(Expense)
-admin.site.register(Provider)
+admin.site.register(Provider, ProveedorAdmin)
 
 
 # Register your models here.
--- income/admin.py
+++ income/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Cliente, Income
 
-admin.site.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('client', 'email', 'phone_number', 'owner')
+    search_fields = ('client', 'owner__username')
+    ordering = ('-owner',)
+
+admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Income)
